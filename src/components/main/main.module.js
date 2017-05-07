4ef41969cf6d6a83a27917f6d1f1a206e9651fd1
Lee Saxby <lee.saxby82@gmail.com>
2017-05-07 14:01:16 -0400

    Code tidy

--- src/components/main/main.module.js
+++ src/components/main/main.module.js
@@ -3,14 +3,12 @@ import router from 'angular-ui-router';
 import welcome from './welcome/welcome.component.js';
 
 export default angular.module('main', [router])
-		.component('welcome', welcome)
-       .config(function ($stateProvider) {
-             
-              
-               $stateProvider
-                      .state('main', {
-                             url: '/main',
-                             component: 'welcome'
-                      })
-        })
-    .name;
+        .component('welcome', welcome)
+        .config(function ($stateProvider) {
+            stateProvider
+                .state('main', {
+                    url: '/main',
+                    component: 'welcome'
+                })
+        }).name;
+        
